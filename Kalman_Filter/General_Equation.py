b31e8f6190dfb1265426ae6c12ed0d9c44956cab
siddhantgupta99 <siddhantgupta9999@gmail.com>
2021-07-27 16:59:21 -0400

    Added predict or motion update function

--- Kalman_Filter/General_Equation.py
+++ Kalman_Filter/General_Equation.py
@@ -5,11 +5,11 @@ from math import *
  
 def f(mu, sigma2, x):
     return 1/sqrt(2*pi*sigma2) * exp(-0.5* ( (x-mu)**2 / sigma2))
-
+# In order to maximize the output of the gaussian function the value of x should be set to mu.
 print (f(10.,4.,10.))
 
 
-# Update function takes two inputs which are prior and the measurement and outputs the posterior
+'''Update function takes two inputs which are prior and the measurement and outputs the posterior'''
 def update(mean1, var1, mean2, var2):
     new_mean = (mean1*var2+mean2*var1)/(var1 + var2)
     new_var = 1/(1/var1 + 1/var2)
@@ -17,6 +17,12 @@ def update(mean1, var1, mean2, var2):
 
 print (update(10.,4.,12., 4.))
 
-# In order to maximize the output of the gaussian function the value of x should be set to mu.
+'''Predict function or motion update takes the prior input and motion's mean and variance and outputs new mean and variance'''
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+print (predict(10., 4., 12., 4.))
 
 
