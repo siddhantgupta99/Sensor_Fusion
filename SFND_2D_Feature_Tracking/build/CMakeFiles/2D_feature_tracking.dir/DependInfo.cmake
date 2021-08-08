b0eae1edb6ba8f4922109aeb77eabc3caff67575
siddhantgupta99 <siddhantgupta9999@gmail.com>
2021-08-08 17:21:59 -0400

    Added midterm project

---
+++ SFND_2D_Feature_Tracking/build/CMakeFiles/2D_feature_tracking.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/siddhantgupta/Desktop/My Documents/Projects&Research/Udacity Sensor Fusion/Camera/SFND_2D_Feature_Tracking/src/MidTermProject_Camera_Student.cpp" "CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o" "gcc" "CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o.d"
+  "/Users/siddhantgupta/Desktop/My Documents/Projects&Research/Udacity Sensor Fusion/Camera/SFND_2D_Feature_Tracking/src/matching2D_Student.cpp" "CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o" "gcc" "CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SFND_2D_Feature_Tracking/build/CMakeFiles/2D_feature_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "2D_feature_tracking"
+  "2D_feature_tracking.pdb"
+  "CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o"
+  "CMakeFiles/2D_feature_tracking.dir/src/MidTermProject_Camera_Student.cpp.o.d"
+  "CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o"
+  "CMakeFiles/2D_feature_tracking.dir/src/matching2D_Student.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2D_feature_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
